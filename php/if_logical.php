a8b301d368110cc13858fb5abdd0437907e20fd4
travtex <tflatt74@gmail.com>
2014-02-06 13:59:18 -0600

    if_logical exercise file added

---
+++ php/if_logical.php
@@ -0,0 +1,44 @@
+<?php 
+
+$x = 0;
+$y = 5;
+$z = 10;
+
+// if $x < $y < $z then echo "{$x} < {$y} < {$z}\n";
+
+if (($x < $y) && ($y < $z)) {
+	echo "{$x} < ${y} < {$z}\n";
+}
+
+// if $x is greater than 0 OR less than 10, echo the result as a sentence
+// "$x is greater than 0 OR less than 10".
+
+if (($x > 0) || ($x < 10)) {
+	echo "{$x} is greater than 0 OR less than 10\n";
+}
+
+// repeat the if statement for $y and $z.
+
+if (($y > 0) || ($y < 10)) {
+		echo "{$y} is greater than 0 OR less than 10\n";
+	}
+
+if (($z > 0) || ($z < 10)) {
+		echo "{$z} is greater than 0 OR less than 10\n";
+	}
+
+// If $x is greater than 0 AND less than 10, echo the result as a sentence.
+
+if (($x > 0) && ($x < 10)) {
+	echo "${x} is greater than 0 AND less than 10.\n";
+}
+
+if (($y > 0) && ($y < 10)) {
+	echo "${y} is greater than 0 AND less than 10.\n";
+}
+
+if (($z > 0) && ($z < 10)) {
+	echo "${z} is greater than 0 AND less than 10.\n";
+}
+
+?>
