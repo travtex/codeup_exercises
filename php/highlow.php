094096db14fc923d14685a4fe933728cb72cecef
vagrant <vagrant@quantal.codeup.dev>
2014-02-07 10:51:12 -0600

    Added guess tracking

--- php/highlow.php
+++ php/highlow.php
@@ -6,6 +6,7 @@ fwrite(STDOUT, PHP_EOL . "I'm thinking of a number between 1 and 100." . PHP_EOL
 fwrite(STDOUT, "Guess the number: ");
 
 $user_guess = fgets(STDIN);
+$guesses = 1;
 
 while (($user_guess < 1) || ($user_guess > 100)) {
 	fwrite(STDOUT, PHP_EOL . "You're not even trying!" . PHP_EOL);
@@ -24,16 +25,19 @@ do {
 			fwrite(STDOUT, PHP_EOL . "That's too high." . PHP_EOL);
 			fwrite(STDOUT, "Try again: ");
 			$user_guess = fgets(STDIN);
+			$guesses++;
 
 		}  else {
 				fwrite(STDOUT, PHP_EOL . "That's too low." . PHP_EOL);
 				fwrite(STDOUT, "Try again: ");
 				$user_guess = fgets(STDIN);
+				$guesses++;
 		}
 	} while(($user_guess < $number) || ($user_guess > $number));
 
 fwrite(STDOUT, PHP_EOL . "That's it!!" . PHP_EOL . "You're BRILLIANT!!" .
-			"Or... lucky, or something!" . PHP_EOL);
+			"Or... lucky, or something!" . PHP_EOL . "It took you " . $guesses . " tries." 
+			. PHP_EOL);
 
 exit(0);
 
