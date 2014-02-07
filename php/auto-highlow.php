602ad7c4d4998e5b355c478b2f2bdf0a35a44792
vagrant <vagrant@quantal.codeup.dev>
2014-02-07 15:26:56 -0600

    debugging auto highlow

--- php/auto-highlow.php
+++ php/auto-highlow.php
@@ -1,50 +1,50 @@
 <?php
 
 fwrite(STDOUT, "Enter the lowest number for the computer to guess: ");
-$low_guess = fgets(STDIN);
+$low_guess = trim(fgets(STDIN));
 
-/*if (is_numeric($low_guess)) {
-	continue;
-	} else {
-		fwrite(STDOUT, "That isn't a number!  Try again: ");
-		$low_guess = fgets(STDIN);
+// Validate low number
+
+while (!is_numeric($low_guess)) {
+	fwrite(STDOUT, "That isn't a number! Try again: ");
+	$low_guess = trim(fgets(STDIN));
 }
-*/
+
 fwrite(STDOUT, "Enter the highest number for the computer to guess: ");
-$high_guess = fgets(STDIN);
+$high_guess = trim(fgets(STDIN));
 
-/*if ((is_int($high_guess)) && ($high_guess > $low_guess)) {
-	continue;
-} elseif ($high_guess <= $low_guess) {
-	fwrite(STDOUT, "Must enter a value greater than " . $low_guess . "!  Try again: ");
-	$high_guess = fgets(STDIN);
-} else {
-	fwrite(STDOUT, "That isn't a number!  Try again: ");
-	$high_guess = fgets(STDIN);
+// Validate high number
+
+while (!is_numeric($high_guess) || intval($high_guess) <= intval($low_guess)) {
+	fwrite(STDOUT, "Invalid entry!  Please enter a number higher than {$low_guess}: ");
+	$high_guess = trim(fgets(STDIN));
 }
-*/
+
+// Set initial random number and initial computer guess
+
 $number = mt_rand(intval($low_guess), intval($high_guess));
 $user_guess = mt_rand(intval($low_guess), intval($high_guess));
 $guesses = 1;
 
 fwrite(STDOUT, "Computer guesses " . $user_guess . PHP_EOL);
 
+// Computer guesses until correct
+
 do {
 
 	if ($user_guess > $number){
 			fwrite(STDOUT, PHP_EOL . "That's too high." . PHP_EOL);
 			$high_guess = ($user_guess - 1);
-			$user_guess = mt_rand(intval($low_guess), intval($high_guess));
-			fwrite(STDOUT, "Computer guesses " . $user_guess . PHP_EOL);
-			$guesses++;
+			
 
 		}  else {
 				fwrite(STDOUT, PHP_EOL . "That's too low." . PHP_EOL);
 				$low_guess = ($user_guess + 1);
-				$user_guess = mt_rand(intval($low_guess), intval($high_guess));
-				fwrite(STDOUT, "Computer guesses " . $user_guess . PHP_EOL);
-				$guesses++;
+				
 		}
+					$user_guess = mt_rand(intval($low_guess), intval($high_guess));
+					fwrite(STDOUT, "Computer guesses " . $user_guess . PHP_EOL);
+					$guesses++;
 	} while(($user_guess < $number) || ($user_guess > $number));
 
 fwrite(STDOUT, PHP_EOL . "That's it!!" . PHP_EOL . "You're BRILLIANT!! " .
