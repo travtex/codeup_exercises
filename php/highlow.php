65dd16fd188d121c10864b9fb085d49a15f7f97a
travtex <tflatt74@gmail.com>
2014-02-07 14:59:49 -0600

    Updated customizable high low

--- php/highlow.php
+++ php/highlow.php
@@ -1,33 +1,60 @@
 <?php
 
-$number = mt_rand(1,100);
+$number = 0;
+$low_val = 0;
+$high_val = 0;
 
-fwrite(STDOUT, PHP_EOL . "I'm thinking of a number between 1 and 100." . PHP_EOL);
-fwrite(STDOUT, "Guess the number: ");
+// Optional initial arguments
+
+if (($argc > 2) && is_numeric($argv[1]) && is_numeric($argv[2])) {
+
+	$number = mt_rand($argv[1],$argv[2]);
+
+	$low_val = intval($argv[1]);
+	$high_val = intval($argv[2]);
+	fwrite(STDOUT, PHP_EOL . "Using custom values.");
+} else {
+		// Default values if arguments not entered
+		$number = mt_rand(1,100);
+		$low_val = 1;
+		$high_val = 100;
+		fwrite(STDOUT, PHP_EOL . "Using default values.  Enter valid low and high values as arguments to customize.");
+}
+
+//   Testing that values are set properly
+
+//   echo $low_val . PHP_EOL . $high_val . PHP_EOL . $number;
+
+
+
+
+fwrite(STDOUT, PHP_EOL . "Guess the number, between " . $low_val . " and " . $high_val . ": ");
 
 $user_guess = fgets(STDIN);
 $guesses = 1;
 
-do {
-	// Ensure valid number entered.
-	if (($user_guess < 1) || ($user_guess > 100)) {
-		fwrite(STDOUT, PHP_EOL . "You're not even trying!" . PHP_EOL);
-		fwrite(STDOUT, "Try again: ");
-		$user_guess = fgets(STDIN);
-	} // Give higher or lower hints
-		elseif($user_guess > $number){
-			fwrite(STDOUT, PHP_EOL . "That's too high." . PHP_EOL);
-			fwrite(STDOUT, "Try again: ");
+// Testing that values are set properly
+
+// echo $low_val . PHP_EOL . $high_val . PHP_EOL . $number . PHP_EOL . $user_guess . PHP_EOL;
+
+while ($user_guess < $number || $user_guess > $number) {
+	
+	if ($user_guess < $low_val || $user_guess > $high_val) {
+		fwrite(STDOUT, PHP_EOL . "Are you even trying?  That's not in range!" . PHP_EOL .
+			"Guess again: ");
+
+	} elseif ($user_guess > $number) {
+		fwrite(STDOUT, PHP_EOL . "That's too high.  Try again: ");
+
+	} else {
+		fwrite(STDOUT, PHP_EOL . "That's too low.  Try again: ");
+
+	}
 			$user_guess = fgets(STDIN);
 			$guesses++;
+}
 
-		}  else {
-				fwrite(STDOUT, PHP_EOL . "That's too low." . PHP_EOL);
-				fwrite(STDOUT, "Try again: ");
-				$user_guess = fgets(STDIN);
-				$guesses++;
-		}
-	} while(($user_guess < $number) || ($user_guess > $number));
+
 // Output the number of guesses
 fwrite(STDOUT, PHP_EOL . "That's it!!" . PHP_EOL . "You're BRILLIANT!! " .
 			"Or... lucky, or something!" . PHP_EOL . "It took you " . $guesses . " tries." 
