d16ab61c7dba11024071c16a5c407e846524091c
vagrant <vagrant@quantal.codeup.dev>
2014-02-10 10:51:41 -0600

    added foreach.php

--- php/foreach.php
+++ php/foreach.php
@@ -7,34 +7,27 @@ $things = ['Sgt. Pepper', "11", null, [1,2,3], 3.14, "12 + 7", false, (string) 1
 // Parse array and output data type.
 
 foreach ($things as $thing){
-	if (is_int($thing)) {
-		fwrite(STDOUT, "{$thing} is an integer." . PHP_EOL);
-	} 
-		elseif (is_float($thing)) {
-			fwrite(STDOUT, "{$thing} is a float." . PHP_EOL);
-	} 
-		elseif (is_bool($thing)) {
-			
-			fwrite(STDOUT, "{$thing} is boolean." . PHP_EOL);
-	} 
-		elseif (is_array($thing)) {
-			// $my_array = implode(",", $thing);
-			foreach ($thing as $thinglet) {
-				fwrite(STDOUT, "{$thinglet}.. ");
-			} fwrite(STDOUT, " is an array." . PHP_EOL);
-
-			// fwrite(STDOUT, "{$my_array} is an array." . PHP_EOL);
-	} 
-		elseif (is_null($thing)) {
-			fwrite(STDOUT, "{$thing} is null." . PHP_EOL);
-	} 
-		elseif (is_string($thing)) {
-			fwrite(STDOUT, "${thing} is a string." . PHP_EOL);
-	} 
-		else {
-			fwrite(STDOUT, "I don't know what {$thing} is!" . PHP_EOL);
+	if (is_int($thing)){
+		fwrite(STDOUT, "{$thing} is an integer.");
 	}
-
-}
-
+		elseif (is_float($thing)){
+			fwrite(STDOUT, "{$thing} is a float.");
+		}
+		elseif (is_bool($thing)){
+			fwrite(STDOUT, "(Here Be Booleans.)");
+		}
+		elseif (is_array($thing)){
+			foreach($thing as $thinglet){
+				fwrite(STDOUT, "{$thinglet}..");
+			}
+				fwrite(STDOUT, " is in an array.");
+		}
+		elseif (is_null($thing)){
+			fwrite(STDOUT, " ... NoThInGnEsS ...");
+		}
+		elseif (is_string($thing)){
+			fwrite(STDOUT, "\"{$thing}\" is a string.");
+		}
+	fwrite(STDOUT, PHP_EOL);
+	}
 ?>
