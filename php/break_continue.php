20e3c2dfa16f173ffe3a0a971b2f36478651af92
travtex <tflatt74@gmail.com>
2014-02-10 14:33:19 -0600

    added break_continue.php

---
+++ php/break_continue.php
@@ -0,0 +1,29 @@
+<?php
+
+for ($i = 0; $i <= 100; $i++) {
+	
+	fwrite(STDOUT, $i);
+		if ($i % 2 == 0) {
+			fwrite(STDOUT, " <--EVEN NUMBER!" . PHP_EOL);
+			continue;
+		} 
+
+		fwrite(STDOUT, PHP_EOL);
+	}
+
+fwrite (STDOUT, PHP_EOL);
+
+for ($i = 0; $i <= 100; $i++) {
+	fwrite (STDOUT, $i . ".. ");
+		if ($i >= 10) {
+			fwrite(STDOUT, "aaaaaand BREAK!" . PHP_EOL);
+			break;
+	}
+}
+
+exit(0);
+
+?>
+
+
+
