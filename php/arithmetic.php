4401052378b21c0a4ffe484f4052d123f09d9e90
vagrant <vagrant@quantal.codeup.dev>
2014-02-11 11:01:01 -0600

    added arithmetic.php

---
+++ php/arithmetic.php
@@ -0,0 +1,37 @@
+<?php
+
+if($argc > 1) {
+	$a = $argv[1];
+	$b = $argv[2];
+} else {
+	$a = 100;
+	$b = 10;
+}
+
+function add($num1, $num2) {
+    fwrite(STDIN, ($num1 + $num2) . PHP_EOL);
+}
+
+function subtract($num1, $num2) {
+	fwrite(STDIN, ($num1 - $num2) . PHP_EOL);
+}
+
+function multiply($num1, $num2) {
+	fwrite(STDIN, ($num1 * $num2) . PHP_EOL);
+}
+
+function divide($num1, $num2) {
+	fwrite(STDIN, ($num1 / $num2) . PHP_EOL);
+}
+
+function mod($num1, $num2) {
+	fwrite(STDIN, ($num1 % $num2) . PHP_EOL);
+}
+
+add($a, $b);
+subtract($a, $b);
+multiply($a, $b);
+divide($a, $b);
+mod($a, $b);
+
+?>
