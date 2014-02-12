f959b2a59927bea63026efd393115dd624815d85
travtex <tflatt74@gmail.com>
2014-02-12 13:33:27 -0600

    finished basic assign functionality in todo.php

--- php/codeup_todo_list/todo.php
+++ php/codeup_todo_list/todo.php
@@ -10,7 +10,8 @@ function list_items($items)
 
     $list_view = "";
 
-    foreach ($items as $key => $item) {
+    foreach ($items as $key => $item) 
+    {
         $user_key = $key + 1;
         $list_view = $list_view . "[" . $user_key . "] {$item}" . PHP_EOL;
     }
@@ -29,8 +30,17 @@ function get_input($upper = FALSE)
     
 }
 
+function assign_todo() 
+{
+    $items = func_get_args();
+    $task = rand(0, (count($items) - 1));
+    return $task;
+
+}
+
 // The loop!
-do {
+do 
+{
     // Iterate through list items
     // foreach ($items as $key => $item) {
     //     // Display each item and a newline
@@ -40,7 +50,7 @@ do {
     echo PHP_EOL . list_items($items) . PHP_EOL;
 
     // Show the menu options
-    echo '(N)ew item, (R)emove item, (Q)uit : ';
+    echo '(N)ew item, (R)emove item, (A)ssign random item, (Q)uit : ';
 
     // Get the input from user
     // Use trim() to remove whitespace and newlines
@@ -65,10 +75,52 @@ do {
         unset($items[$key - 1]);
         $items = array_values($items);
     }
+    elseif ($input == 'A')
+    {
+
+        if ($items) 
+        {
+            while ($input != 'Y' || $input != 'N') 
+            {
+                // Select a random task and give option to remove it
+                $input = '';
+                $key = call_user_func_array('assign_todo', $items);
+
+                echo PHP_EOL . 'Your assigned task is: ' . $items[$key]
+                . '.' . PHP_EOL;
+
+                echo 'Have you finished this todo item? (Y/N): ';
+
+                $input = get_input(TRUE);
+         
+
+                if ($input == 'Y') 
+                {
+                    echo PHP_EOL . "Great!  Let's check that one off the list." . PHP_EOL;
+                    unset($items[$key]);
+                    $items = array_values($items);
+                    break;
+
+                }
+                elseif ($input == 'N') 
+                {
+                    echo PHP_EOL . "Why not?  Quit being a slacker." . PHP_EOL;
+                    break;
+                }
+
+            }
+        } 
+        else 
+        {
+            echo PHP_EOL . "There are no list items to assign." . PHP_EOL;
+    }
+    
 
 // Exit when input is (Q)uit
 
-} while ($input != 'Q');
+    } 
+}
+    while ($input != 'Q');
 
 echo "Goodbye!\n";
 
