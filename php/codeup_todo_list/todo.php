5345823351a0840fe8726cb2ed17473c0b6a7a90
Travis Flatt <tflatt74@gmail.com>
2014-02-11 19:59:01 -0600

    added functions to todo.php

--- php/codeup_todo_list/todo.php
+++ php/codeup_todo_list/todo.php
@@ -3,28 +3,61 @@
 // Create array to hold list of todo items
 $items = array();
 
+// List array items
+
+function list_items($items)
+{
+
+    $list_view = "";
+
+    foreach ($items as $key => $item) {
+        $user_key = $key + 1;
+        $list_view = $list_view . "[" . $user_key . "] {$item}" . PHP_EOL;
+    }
+
+    return $list_view;
+}
+
+// Get user input
+
+function get_input()
+{
+    $user_input = trim(fgets(STDIN));
+
+    if (ctype_lower($user_input))
+    {
+        return strtoupper($user_input);
+    }
+    else 
+    {
+        return $user_input;
+    }
+}
+
 // The loop!
 do {
     // Iterate through list items
-    foreach ($items as $key => $item) {
-        // Display each item and a newline
-        echo "[" . ($key + 1) . "] {$item}\n";
-    }
+    // foreach ($items as $key => $item) {
+    //     // Display each item and a newline
+    //     echo "[" . ($key + 1) . "] {$item}\n";
+    // }
+
+    echo PHP_EOL . list_items($items) . PHP_EOL;
 
     // Show the menu options
     echo '(N)ew item, (R)emove item, (Q)uit : ';
 
     // Get the input from user
     // Use trim() to remove whitespace and newlines
-    $input = strtolower(trim(fgets(STDIN)));
+    $input = get_input();
 
     // Check for actionable input
-    if ($input == 'n') {
+    if ($input == 'N') {
         // Ask for entry
         echo 'Enter item: ';
         // Add entry to list array
         $items[] = trim(fgets(STDIN));
-    } elseif ($input == 'r') {
+    } elseif ($input == 'R') {
         // Remove which item?
         echo 'Enter item number to remove: ';
         // Get array key
@@ -34,7 +67,7 @@ do {
         $items = array_values($items);
     }
 // Exit when input is (Q)uit
-} while ($input != 'q');
+} while ($input != 'Q');
 
 // Say Goodbye!
 echo "Goodbye!\n";
