d3b8a88cf1e015c097c07f2526921fd2cc66352c
travtex <tflatt74@gmail.com>
2014-02-12 10:16:32 -0600

    internal functions edit

--- php/internal_functions.php
+++ php/internal_functions.php
@@ -6,9 +6,10 @@ $array = array(1,2,3);
 
 // Create a funciton that checks if a variable is set or empty, and display "$variable_name is SET|EMPTY"
 
-function var_is_set($variable) {
+function var_is_set($variable) 
+{
+	return isset($variable) ? 'SET' : 'EMPTY';
 
-	return isset($variable) ?  'SET' :  'EMPTY';
 }
 
 // TEST: If var $nothing is set, display '$nothing is SET'
