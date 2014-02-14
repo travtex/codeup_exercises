a5d0b18428e846636a2310d4b79fee9fb363b493
travtex <tflatt74@gmail.com>
2014-02-14 13:36:34 -0600

    fixed bidirectional pig latin function

--- php/coding_challenge/pig_latin.php
+++ php/coding_challenge/pig_latin.php
@@ -30,12 +30,49 @@ function pig_latin($english)
 	return $pigword;
 }
 
-echo PHP_EOL . "Enter the english phrase to convert: ";
-$english = get_input();
+function to_english($piglatin)
+{
+	$pig_array = explode(' ', $piglatin);
+	$english_words = [];
 
-$pig_latin = pig_latin($english);
+	foreach($pig_array as $word)
+		{
+			$first = substr($word, 0, 1);
+			if(ctype_upper($first))
+			{
+				$first = strtolower($first);
+				$new_cap = strtoupper(substr($word, -3, 1));
+			}
+			else 
+			{
+				$new_cap = (substr($word, -3, 1));
+			}
 
-echo PHP_EOL . implode(' ', $pig_latin) . PHP_EOL;
+			$eng_word = substr($word, 1, -3);
+			$english_words[] = $new_cap . $first . $eng_word;
+		}	
+		return $english_words;
+}
+
+echo PHP_EOL . "Do you want to start with (E)nglish or (P)ig Latin?: ";
+$choose = get_input(TRUE);
+
+switch($choose)
+{
+	case 'E':
+		echo PHP_EOL . "Enter the english phrase to convert: ";
+		$english = get_input();
+		$pig_latin = pig_latin($english);
+		echo PHP_EOL . implode(' ', $pig_latin) . PHP_EOL;
+		break;
+
+	case 'P':
+		echo PHP_EOL. "Enter the pig-latin phrase to convert: ";
+		$piggy = get_input();
+		$english = to_english($piggy);
+		echo PHP_EOL . implode(' ', $english) . PHP_EOL;
+}
+
 
 exit(0);
 
