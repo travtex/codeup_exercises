95eb7b1de88b2d2ad00d3c11f03bc1ac9a09b2ab
Travis Flatt <tflatt74@gmail.com>
2014-02-16 23:52:06 -0600

    got morse2 working

--- php/coding_challenge/morse2.php
+++ php/coding_challenge/morse2.php
@@ -29,8 +29,207 @@ function import_data($filename)
 fwrite(STDIN, PHP_EOL . "Which morse code file do you want to translate?: ");
 $decode_this_file = import_data(get_input());
 
+$decode_this_file .= ' ';
+
+// var_dump($decode_this_file);
+
 $decoded = '';
 
-var_dump($decode_this_file);
+$strlen = strlen($decode_this_file);
+
+$working_string = '';
+
+for($i = 0; $i < $strlen; $i++)
+{
+	if($decode_this_file{$i} == ' ')
+	{
+		
+		switch ($working_string)
+		{
+			case '-':
+				$decoded .= 'T';
+				break;
+			case '--':
+				$decoded .= 'M';
+				break;
+			case '---':
+				$decoded .= 'O';
+				break;
+			case '--.':
+				$decoded .= 'G';
+				break;
+			case '--.-':
+				$decoded .= 'Q';
+				break;
+			case '--..': 
+				$decoded .= 'Z';
+				break;
+			case '-.':
+				$decoded .= 'N';
+				break;
+			case '-.-':
+				$decoded .= 'K';
+				break;
+			case '-.--':
+				$decoded .= 'Y';
+				break;
+			case '-.-.':
+				$decoded .= 'C';
+				break;
+			case '-..':
+				$decoded .= 'D';
+				break;
+			case '-..-':
+				$decoded .= 'X';
+				break;
+			case '-...':
+				$decoded .= 'B';
+				break;
+			case '.':
+				$decoded .= 'E';
+				break;
+			case '.-':
+				$decoded .= 'A';
+				break;
+			case '.--':
+				$decoded .= 'W';
+				break;
+			case '.---':
+				$decoded .= 'J';
+				break;
+			case '.--.':
+				$decoded .= 'P';
+				break;
+			case '.-.':
+				$decoded .= 'R';
+				break;
+			case '.-..':
+				$decoded .= 'L';
+				break;
+			case '..':
+				$decoded .= 'I';
+				break;
+			case '..-':
+				$decoded .= 'U';
+				break;
+			case '..-.':
+				$decoded .= 'F';
+				break;
+			case '...':
+				$decoded .= 'S';
+				break;
+			case '...-':
+				$decoded .= 'V';
+				break;
+			case '....':
+				$decoded .= 'H';
+				break;
+
+			// Numbers
+
+			case '-----':
+				$decoded .= '0';
+				break;
+			case '----.':
+				$decoded .= '9';
+				break;
+			case '---..':
+				$decoded .= '8';
+				break;
+			case '--...':
+				$decoded .= '7';
+				break;
+			case '-....':
+				$decoded .= '6';
+				break;
+			case '.----':
+				$decoded .= '1';
+				break;
+			case '..---':
+				$decoded .= '2';
+				break;
+			case '...--':
+				$decoded .= '3';
+				break;
+			case '....-':
+				$decoded .= '4';
+				break;
+			case '.....':
+				$decoded .= '5';
+				break;
+
+			// Other characters
+
+			case '---...':
+				$decoded .= ':';
+				break;
+			case '--..--':
+				$decoded .= ',';
+				break;
+			case '-.--.-':
+				$decoded .= '\)';
+				break;
+			case '-.-.--':
+				$decoded .= '!';
+				break;
+			case '-.-.-.':
+				$decoded .= ';';
+				break;
+			case '-..-.':
+				$decoded .= '/';
+				break;
+			case '-...-':
+				$decoded .= '=';
+				break;
+			case '-....-':
+				$decoded .= '+';
+				break;
+			case '.-.-.-':
+				$decoded .= '.';
+				break;
+			case '.-..-.':
+				$decoded .= '"';
+				break;
+			case '.-...':
+				$decoded .= '&';
+				break;
+			case '..--.-':
+				$decoded .= '_';
+				break;
+			case '..--..':
+				$decoded .= '?';
+				break;
+			case '...-..-':
+				$decoded .= '$';
+				break;
+
+			// Space character
+
+			case '/':
+				$decoded .= ' ';
+				break;
+
+			// Newline character (NOT WORKING?!)	
+			case "\n":
+				$decoded .= "\n";
+				break;
+
+				
+		}
+		$working_string = '';
+	}
+	else
+	{
+	$working_string .= $decode_this_file{$i};
+	}
+}
+
+fwrite(STDOUT, PHP_EOL . "Your translated morse file reads: " . PHP_EOL . $decoded . PHP_EOL);
+
+exit(0);
+
+//var_dump($decoded);
+// var_dump($string_place);
+//var_dump($decode_this_file{1});
 
 ?>
