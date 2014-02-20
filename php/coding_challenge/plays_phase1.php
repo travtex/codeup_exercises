f44cee406d8c708298d9a9f7550dd91f47a2b68d
Travis Flatt <tflatt74@gmail.com>
2014-02-19 20:58:26 -0600

    plays phase1 work

--- php/coding_challenge/plays_phase1.php
+++ php/coding_challenge/plays_phase1.php
@@ -30,6 +30,20 @@ function get_lines_nothing($character) {
 	}
 }
 
+function get_lines_pygmalion($character) {
+	$lines = '';
+	$my_lines = [];
+	$filename = "../data/pygmalion.txt";
+	$character1 = strtoupper($character) . '\. ';
+	$character2 = strtoupper($character) . '\ \[';
+
+	$lines = file_get_contents($filename);
+
+	if(preg_match_all('~' . $character1 . '(.*?)' . "\n\n" . '~s', $lines, $my_lines)) {
+		return $my_lines[0];
+	} 
+}
+
 // ask user for the character name
 echo PHP_EOL . "Please select a play.\n\n(1)  The Importance of Being Earnest\n(2)  "
 	. "Much Ado About Nothing\n(3)  Pygmalion" . PHP_EOL;
@@ -37,23 +51,33 @@ echo PHP_EOL . "Please select a play.\n\n(1)  The Importance of Being Earnest\n(
 echo PHP_EOL . "Pick a play: ";
 
 $my_play = get_input();
+echo PHP_EOL . "Please enter the character's name: ";
+$my_role = get_input();
 
 switch($my_play) {
 	case '1':
 
-		echo PHP_EOL . "Please enter the character's name: ";
-		$my_role = get_input();
 		$my_lines = get_lines_earnest($my_role);
+		break;
 
-		echo PHP_EOL . "Your character's lines are: " . PHP_EOL;
+	case '2':
 
-		foreach($my_lines as $line){
-			echo $line . PHP_EOL;
-		}
+		$my_lines = get_lines_nothing($my_role);
+		break;
+
+	case '3':
+		$my_lines = get_lines_pygmalion($my_role);
 		break;
 }
 
-// var_dump($my_lines);
+echo PHP_EOL . "Your character's lines are: \n" . PHP_EOL;
 
+foreach($my_lines as $line) {
+	echo $line . PHP_EOL;
+}
+
+exit(0);
+
+// var_dump($my_lines);
 // return all play lines for that character
 ?>
